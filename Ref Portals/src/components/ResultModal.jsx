8a17bf45f78797198e888d981bd02c4c4ec3424b
bibibigg <aerl0706@naver.com>
2024-12-06 21:39:28 +0900

    forwarding refs

---
+++ Ref Portals/src/components/ResultModal.jsx
@@ -0,0 +1,41 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round(1 - remainingTime / (targetTime * 1000)) * 100;
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} second.</strong>
+      </p>
+      <p>
+        You stopped the timer with
+        <strong>{formattedRemainingTime} second left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
+
+export default ResultModal;
+// dialog안에 form을 제출하는 버튼이 있기에 버튼을 통해 form을 제출할 수 있음
+// dialog는 기본적으로 보이지 않기에 open속성을 추가하여 보이게 할 수 있음
+//.toFixed()괄호 안에 숫자만큼 소수점자리를 표기할 수 있음.toFixed(2)는 소수점2자리까지만 표기
--- Ref Portals/src/components/TimerChallange.jsx
+++ Ref Portals/src/components/TimerChallange.jsx
@@ -1,37 +1,57 @@
 import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 export default function TimerChallange({ title, targetTime }) {
   const timer = useRef();
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+  const dialog = useRef();
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function hadleReset() {
+    setTimeRemaining(targetTime * 1000);
+  }
 
   function handleStart() {
-    setTimerStarted(true);
-    timer.current = setTimeout(() => {
-      setTimerExpired(true);
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 10);
+    }, 10);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    clearInterval(timer.current);
+    dialog.current.open();
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? "Stop" : "Start"} Challenge
-        </button>
-      </p>
+    <>
+      <ResultModal
+        ref={dialog}
+        targetTime={targetTime}
+        remainingTime={timeRemaining}
+        onReset={hadleReset}
+      />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
 
-      <p className={timerStarted ? "active" : undefined}>
-        {timerStarted ? "Time is running" : "Timeer inactive"}
-      </p>
-    </section>
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Time is running" : "Timeer inactive"}
+        </p>
+      </section>
+    </>
   );
 }
