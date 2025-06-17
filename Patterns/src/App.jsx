5a2cb6d0318a40d2529415c6c6149a51fa1b226d
bibibigg <aerl0706@naver.com>
2025-06-18 00:30:40 +0900

    Render Props

--- Patterns/src/App.jsx
+++ Patterns/src/App.jsx
@@ -1,7 +1,48 @@
-import Accordion from "./components/Accordion";
-import AccordionContent from "./components/AccordionContent";
-import AccordionTitle from "./components/AccordionTitle";
+import Accordion from "./components/Accordion/Accordion";
+import AccordionContent from "./components/Accordion/AccordionContent";
+import AccordionTitle from "./components/Accordion/AccordionTitle";
+import SearchableList from "./components/SearchableList/SearchableList";
+import savannaImg from "./assets/african-savanna.jpg";
+import amazonImg from "./assets/amazon-river.jpg";
+import caribbeanImg from "./assets/caribbean-beach.jpg";
+import desertImg from "./assets/desert-dunes.jpg";
+import forestImg from "./assets/forest-waterfall.jpg";
+import Place from "./Place";
+
 function App() {
+  const PLACES = [
+    {
+      id: "african-savanna",
+      image: savannaImg,
+      title: "African Savanna",
+      description: "Experience the beauty of nature.",
+    },
+    {
+      id: "amazon-river",
+      image: amazonImg,
+      title: "Amazon River",
+      description: "Get to know the largest river in the world.",
+    },
+    {
+      id: "caribbean-beach",
+      image: caribbeanImg,
+      title: "Caribbean Beach",
+      description: "Enjoy the sun and the beach.",
+    },
+    {
+      id: "desert-dunes",
+      image: desertImg,
+      title: "Desert Dunes",
+      description: "Discover the desert life.",
+    },
+    {
+      id: "forest-waterfall",
+      image: forestImg,
+      title: "Forest Waterfall",
+      description: "Listen to the sound of the water.",
+    },
+  ];
+
   return (
     <main>
       <section>
@@ -36,6 +77,14 @@ function App() {
             </AccordionContent>
           </Accordion.Item>
         </Accordion>
+      </section>
+      <section>
+        <SearchableList items={PLACES} itemKeyFn={(item) => item.id}>
+          {(item) => <Place item={item} />}
+        </SearchableList>
+        <SearchableList items={["item1", "item2"]} itemKeyFn={(item) => item}>
+          {(item) => item}
+        </SearchableList>
       </section>
     </main>
   );
